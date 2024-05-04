105c05e4148a10118ce691dca7a2dbfc53fda608
archa <archa@DESKTOP-T17NCFH>
2024-05-04 09:45:13 -0500

    Add project files.

---
+++ example2.cs
@@ -0,0 +1,30 @@
+﻿namespace CAConstructor
+{
+    class wallet
+    {
+        double balance, itemprice;
+        string[] itemsAvailable;
+        
+            public wallet()
+        {
+            balance = 20000;
+            itemsAvailable =new string[] { "Mobile , TV , Laptop , Desktop" };
+        }
+        public void Display()
+        {
+            Console.WriteLine("Wallet Balance is " + balance,"ItemsAvailable is " + itemsAvailable);
+            Console.WriteLine($"items available is:{itemsAvailable},item price is {itemprice}");
+        }
+ 
+    }
+    class program
+    {
+        static void Main(string[] args) 
+        {
+            wallet wallet = new wallet();
+            wallet.Display();
+            wallet itemprice =new wallet ("Mobile",30000);
+            itemprice.Display();
+        }
+    }
+}
